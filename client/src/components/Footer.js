c200c69015ea185fb8d760ea3f7428b59f25a9cf
Ely Alamillo <ely0013@gmail.com>
2018-01-18 11:22:05 -0600

    added contact us email

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,9 +5,10 @@ const Footer = () => {
   return (
     <footer className="custom-footer">
       <h1> 2017 &#169; <a href="whaatever lik" target="blank"> Speech Trainer</a>
-        &nbsp;| Contact Us </h1>
+        &nbsp;|  <a href="mailto:speechtrainingorg@gmail.com?subject=Contact_Us">Contact Us</a></h1>
     </footer>
   );
 };
+
 
 export default Footer;
